26b355c825243d0bba10d39c77ff3beaecf571c1
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-07-06 16:05:21 +0530

    [feat] add new theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-// import { useState } from 'react'
 import "./App.css";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  // Navigate,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Display from "./modules/screen/pages/display";
 import Admin from "./modules/admin/pages/admin";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "./firebase/config";
+import Display2 from "./modules/screen/pages/display2";
+
+type ThemeData = {
+  theme: number;
+  layoutStyle: string;
+};
 
 function App() {
+  const [themeData, setThemeData] = useState<ThemeData>();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userCollectionRef = collection(db, "theme");
+        const q = query(userCollectionRef);
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+          querySnapshot.docs.forEach((doc) => {
+            setThemeData(doc.data() as ThemeData);
+          });
+        });
+
+        return () => unsubscribe(); // Unsubscribe from the snapshot listener when component unmounts
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    if (themeData) {
+      const root = document.documentElement;
+      root.style.setProperty(
+        "--bg-image",
+        `url(../src/modules/screen/assets/${
+          themeData.theme === 1 ? "bg.png" : "bg.webp"
+        })`
+      );
+      root.style.setProperty(
+        "--font-family",
+        themeData.theme === 1 ? "ndot-47" : "CalSans-SemiBold"
+      );
+    }
+  }, [themeData]);
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Display />,
+      element: themeData?.theme === 1 ? <Display /> : <Display2 />,
     },
     {
       path: "/admin",
       element: <Admin />,
     },
   ]);
+
   return (
     <>
       <RouterProvider router={router} />
--- src/modules/screen/services/api.ts
+++ src/modules/screen/services/api.ts
@@ -1,19 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
+import { IndexData } from "../../../enum";
 
 export const getWeather = async () => {
-    try {
-        const response = await axios.get(
-            "https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/188802?apikey=258od518cXWa0GcSX1ShgFL6EpY3DTUt"
-        );
+  try {
+    const response = await axios.get(
+      "https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/188802?apikey=258od518cXWa0GcSX1ShgFL6EpY3DTUt"
+    );
 
-        // Check if response status is not OK
-        if (response.status !== 200) {
-            throw new Error("Failed to fetch weather data");
-        }
-
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        throw error;
+    // Check if response status is not OK
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch weather data");
     }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+};
+export const getNIFTY = async () => {
+  try {
+    const response = await axios.get("https://www.nseindia.com/api/allIndices");
+
+    // Check if response status is not OK
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch weather data");
+    }
+    const data = response.data;
+    // Find the first entry with the key "BROAD MARKET INDICES"
+    const broadMarketIndex = data.find(
+      (item: IndexData) => item.key === "BROAD MARKET INDICES"
+    );
+
+    return broadMarketIndex;
+  } catch (error) {
+    console.error("Error fetching NIFTY data:", error);
+    throw error;
+  }
 };
